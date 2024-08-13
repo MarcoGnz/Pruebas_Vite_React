0892bea99acb903c396d4c206ff952f79d5bf32b
marrco.gonzalez <marrco.gonzalez@gmail.com>
2024-08-13 06:59:11 +0000

    Practica APP alquiler

--- src/App_DatosJson_components.jsx
+++ src/App_DatosJson_components.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import "./App.css";
-import 'bootstrap/dist/css/bootstrap.min.css';
+
 import Cartas from "./components/carts";
 
 function App() {
